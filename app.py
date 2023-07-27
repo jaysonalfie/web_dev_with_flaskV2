98aa6922081a428bf00f154bcf264cc6c0df0dba
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2023-07-27 23:40:35 +0300

    added API requirements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template#using template
+from flask import Flask, render_template, jsonify #using template
 #creating a flask app
 app = Flask(__name__)
 
@@ -35,6 +35,11 @@ JOBS = [
 def hello_world():
      return  render_template('home.html',
                              jobs = JOBS) 
+     
+     
+@app.route("/api/jobs")
+def list_jobs():
+     return jsonify(JOBS)
 
 
 if __name__ == "__main__":
