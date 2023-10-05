313128e0e00d7c5e24490693ba75509ed735d0df
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2023-10-05 12:25:06 +0300

    added and view db on site

--- app.py
+++ app.py
@@ -1,45 +1,63 @@
 from flask import Flask, render_template, jsonify #using template
+from database import engine
+from sqlalchemy import text
 #creating a flask app
 app = Flask(__name__)
 
 #creating python list in which jobs will be stored
-JOBS = [
-     {
-          'id': 1,
-          'title': 'Data analyst',
-          'location': 'Nairobi, Kenya',
-          'salary': 'Ksh 100,000'
-     },
-     {
-          'id': 2,
-          'title': 'Data Scientist',
-          'location': 'Nakuru, Kenya',
-          'salary': 'Ksh 150,000'
-     },
-      {
-          'id': 3,
-          'title': 'Software Engineer',
-          'location': 'Momabasa, Kenya',
+# JOBS = [
+#      {
+#           'id': 1,
+#           'title': 'Data analyst',
+#           'location': 'Nairobi, Kenya',
+#           'salary': 'Ksh 100,000'
+#      },
+#      {
+#           'id': 2,
+#           'title': 'Data Scientist',
+#           'location': 'Nakuru, Kenya',
+#           'salary': 'Ksh 150,000'
+#      },
+#       {
+#           'id': 3,
+#           'title': 'Software Engineer',
+#           'location': 'Momabasa, Kenya',
           
-     },
-       {
-          'id': 4,
-          'title': 'Backend Engineer',
-          'location': 'Remote',
-          'salary': 'Ksh 180,000'
-     }
-]
+#      },
+#        {
+#           'id': 4,
+#           'title': 'Backend Engineer',
+#           'location': 'Remote',
+#           'salary': 'Ksh 180,000'
+#      }
+# ]
+#function that returns jobs from db
+def load_jobs_from_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("select * from jobs"))
+
+        jobs = []
+        # Converting a SQLAlchemy row object to a dictionary using _asdict()
+        for row in result:
+            jobs.append(row._asdict())
+
+    return jobs
+
+    
+
 
 #registering route to application
 @app.route("/")
 def hello_world():
+     jobs_list = load_jobs_from_db()
      return  render_template('home.html',
-                             jobs = JOBS) 
+                             jobs = jobs_list) 
      
      
 @app.route("/api/jobs")
 def list_jobs():
-     return jsonify(JOBS)
+     jobs_list = load_jobs_from_db()
+     return jsonify(jobs_list)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,20 +1,45 @@
 from sqlalchemy import create_engine, text
+from decouple import config # Import config from decouple
 
-engine = create_engine("mysql+pymysql://root:@127.0.0.1/eveman2_jobs?charset=utf8mb4")
-with engine.connect() as connection:
-    result = connection.execute(text("select * from jobs"))
-    # print("type(result):",type(result))
-    # result_all =result.all()
-    # print("type(result.all()):", type(result_all))
 
-    result_dicts =[]
-    # converting a sqlalchemy row object to a dictionary using._asdict()
-    result_dicts = [row._asdict() for row in result]
+# Load database configuration from .env file
+DB_HOST = config('DB_HOST', default='127.0.0.1')
+DB_PORT = config('DB_PORT', default='3306')
+DB_USER = config('DB_USER', default='root')
+DB_PASSWORD = config('DB_PASSWORD', default='')
+DB_NAME = config('DB_NAME', default='eveman2_jobs')
+DATABASE_URL = config('DATABASE_URL')  # Load DATABASE_URL from .env
 
-    print(result_dicts)
+
+engine = create_engine(DATABASE_URL)
+
+
+# with engine.connect() as connection:
+#     result = connection.execute(text("select * from jobs"))
+#     # print("type(result):",type(result))
+#     # result_all =result.all()
+#     # print("type(result.all()):", type(result_all))
+
+#     result_dicts =[]
+#     # converting a sqlalchemy row object to a dictionary using._asdict()
+#     result_dicts = [row._asdict() for row in result]
+
+    # print(result_dicts)
 
     # first_result = result_all[0]
     # print("type(first_result):",type(first_result))
     # first_result_dict = result_all[0]._asdict()
     # print("type(first_result_dict):",type(first_result_dict))
     # print(first_result_dict)
+def load_jobs_from_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("select * from jobs"))
+
+        jobs = []
+        # Converting a SQLAlchemy row object to a dictionary using _asdict()
+        for row in result:
+            jobs.append(row._asdict())
+
+    return jobs
+
+   
